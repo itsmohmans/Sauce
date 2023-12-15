a00ed4ee189f47d244d65a8898e3328013fe568a
Baraa Al-Masri <pub@mbaraa.com>
2023-12-15 03:05:36 +0300

    Add getCurrentTabUrl function

--- browserAction/script.js
+++ browserAction/script.js
@@ -1,1 +1,25 @@
-document.getElementById('myHeading').style.color = 'red'
+const el = document.getElementById("myHeading");
+const buttonElement = document.getElementById("get-sauce-button");
+
+const ingredientsApiUrl = "http://localhost:8000";
+
+async function getSauce() {
+  el.innerHTML = await getCurrentTabUrl();
+}
+
+/**
+ *
+ * @returns{Promise<string>}
+ */
+async function getCurrentTabUrl() {
+  return browser.tabs
+    .query({ currentWindow: true, active: true })
+    .then((tabs) => {
+      const url = tabs[0].url;
+      const noHost = url.substring(url.indexOf(":") + 3);
+      return noHost.substring(0, noHost.indexOf("/") ?? undefined);
+    }, console.error);
+}
+
+el.addEventListener("click", getSauce);
+buttonElement.addEventListener("click", getSauce);
