2ceb19fa56dd31f4d3d2a195e407b1a9f72db0dd
Baraa Al-Masri <pub@mbaraa.com>
2023-12-15 03:46:29 +0300

    Add types and refactor script

--- browserAction/script.js
+++ browserAction/script.js
@@ -1,10 +1,159 @@
-const el = document.getElementById("myHeading");
-const buttonElement = document.getElementById("get-sauce-button");
+class Ingredient {
+  /**
+   * @type{string}
+   */
+  id;
+  /**
+   * @type{string}
+   */
+  name;
+  /**
+   * @type{string}
+   */
+  description;
+  /**
+   * @type{string}
+   */
+  icon;
+}
 
-const ingredientsApiUrl = "http://localhost:8000";
+class Ingredients {
+  /**
+   * @type{Ingredient[]}
+   */
+  ads;
+  /**
+   * @type{Ingredient[]}
+   */
+  analytics;
+  /**
+   * @type{Ingredient[]}
+   */
+  auth;
+  /**
+   * @type{Ingredient[]}
+   */
+  blogs;
+  /**
+   * @type{Ingredient[]}
+   */
+  builders;
+  /**
+   * @type{Ingredient[]}
+   */
+  cdn;
+  /**
+   * @type{Ingredient[]}
+   */
+  cms;
+  /**
+   * @type{Ingredient[]}
+   */
+  compliance;
+  /**
+   * @type{Ingredient[]}
+   */
+  docs;
+  /**
+   * @type{Ingredient[]}
+   */
+  ecommerce;
+  /**
+   * @type{Ingredient[]}
+   */
+  fonts;
+  /**
+   * @type{Ingredient[]}
+   */
+  frameworks;
+  /**
+   * @type{Ingredient[]}
+   */
+  hosts;
+  /**
+   * @type{Ingredient[]}
+   */
+  libraries;
+  /**
+   * @type{Ingredient[]}
+   */
+  monitoring;
+  /**
+   * @type{Ingredient[]}
+   */
+  notifications;
+  /**
+   * @type{Ingredient[]}
+   */
+  other;
+  /**
+   * @type{Ingredient[]}
+   */
+  payments;
+  /**
+   * @type{Ingredient[]}
+   */
+  search;
+  /**
+   * @type{Ingredient[]}
+   */
+  security;
+  /**
+   * @type{Ingredient[]}
+   */
+  servers;
+  /**
+   * @type{Ingredient[]}
+   */
+  storage;
+  /**
+   * @type{Ingredient[]}
+   */
+  widgets;
+  /**
+   * @type{Ingredient[]}
+   */
+  wikis;
+}
 
-async function getSauce() {
-  el.innerHTML = await getCurrentTabUrl();
+class PageData {
+  /**
+   * @type{string}
+   */
+  url;
+  /**
+   * @type{number}
+   */
+  matchingIngredientsCount;
+  /**
+   * @type{Ingredients}
+   */
+  ingredients;
+
+  /**
+   * @param {string} url
+   * @param {number} matchingIngredientsCount
+   * @param {Ingredients} ingredients
+   */
+  constructor(url, matchingIngredientsCount, ingredients) {
+    this.url = url;
+    this.matchingIngredientsCount = matchingIngredientsCount;
+    this.ingredients = ingredients;
+  }
+}
+
+/**
+ * @param {string} ingredientsApiUrl
+ * returns{Promise<PageData>}
+ */
+async function getSauce(ingredientsApiUrl) {
+  const currentTabUrl = await getCurrentTabUrl();
+  return await fetch(`${ingredientsApiUrl}/ingredients?url=${currentTabUrl}`)
+    .then((res) => res.json())
+    .then(
+      (data) => new PageData(data.url, data.matching_ingredients, data.matches),
+      console.error,
+    );
 }
 
 /**
@@ -21,5 +170,15 @@ async function getCurrentTabUrl() {
     }, console.error);
 }
 
-el.addEventListener("click", getSauce);
-buttonElement.addEventListener("click", getSauce);
+function main() {
+  const el = document.getElementById("myHeading");
+  const buttonElement = document.getElementById("refresh-sauce-button");
+  const ingredientsApiUrl = "http://localhost:8000";
+  el.addEventListener("click", () => getSauce(ingredientsApiUrl));
+  buttonElement.addEventListener("click", () => getSauce(ingredientsApiUrl));
+  window.addEventListener("load", async () => {
+    el.innerHTML = JSON.stringify(await getSauce(ingredientsApiUrl));
+  });
+}
+
+main();
